11c797a6b2b9e46f0a0fb3e99991aa622ba2df04
Wade Ashby <mashby6@gatech.edu>
2016-03-08 14:36:36 -0600

    Slight alter to ProcessMain to test.

--- src/ProcessMain.java
+++ src/ProcessMain.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Timer;
 
 import fileUtilities.DownloadSetup;
 import fileUtilities.FileFormatter;
